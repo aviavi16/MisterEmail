dd8cd3ae9529673631bcfab197aa34923c8a6267
aviavi16 <eranis54321@gmail.com>
2024-08-19 23:49:48 +0300

    added email details css

--- src/cmps/EmailDetails.jsx
+++ src/cmps/EmailDetails.jsx
@@ -2,25 +2,35 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router"
 import { emailService } from "../services/emailService"
 import { Link } from "react-router-dom"
+import backIcon from "../assets/imgs/back.png"
 
 export function EmailDetails(){
-    const [emails, setEmails] = useState(null)
+    const [email, setEmail] = useState(null)
     const {id} = useParams()
 
     useEffect(() => {
-        loadEmails()
+        loadEmail()
     }, [id])
         
-    async function loadEmails(){
-        const emailsList = await emailService.getById(id)
-        setEmails(emailsList)
+    async function loadEmail(){
+        const email = await emailService.getById(id)
+        setEmail(email)
     }
 
+    if(!email) return <span> loading in progress... </span>
     return (
-        <section className="email details"> 
-        <span> email details page </span>
-             <pre>  {JSON.stringify(emails, null, 2)}</pre>
-             <Link to='/email'> Back </Link>
+        <section className="email-details"> 
+        <h1 > Subject: { email.subject }</h1>
+        <div className="email-header">
+             
+            <div className="sender-container">
+             <span > { email.sender.name }</span>
+             <span className="sender-email"> {'<'}{ email.sender.email}{'>' }</span>
+             </div>
+             <Link to='/email'>Back <img src= {backIcon} className="back-btn" title="back"/> </Link>
+        </div>
+
+             <pre>  {JSON.stringify(email.body, null, 2)}</pre>
         </section>
     )
 }
